665ad9c27abb286ad0ebf3ec95562c75bc405b59
denis909 <denis909@mail.ru>
2020-11-19 17:33:03 +0300

    n/a

--- BootstrapAsset.php
+++ BootstrapAsset.php
@@ -2,13 +2,15 @@
 
 namespace denis909\bootstrap5;
 
+use yii\web\View;
+
 class BootstrapAsset extends \yii\web\AssetBundle
 {
 
     public $sourcePath = '@vendor/twbs/bootstrap';
 
     public $css = [
-        'css/bootstrap.css'
+        ['css/bootstrap.css', 'position' => View::POS_HEAD]
     ];
 
 }
