665221c3302f5556c816956943df04cca18c94cf
denis909 <denis909@mail.ru>
2020-11-19 17:31:39 +0300

    n/a

---
+++ BootstrapAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace denis909\bootstrap5;
+
+class BootstrapAsset extends \yii\web\AssetBundle
+{
+
+    public $sourcePath = '@vendor/twbs/bootstrap';
+
+    public $css = [
+        'css/bootstrap.css'
+    ];
+
+}
